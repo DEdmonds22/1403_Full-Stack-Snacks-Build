64bf0b9a5a5fb0f044cf905cbcfaff243703f69f
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-27 19:17:11 -0400

    created a models folder and Snack model

---
+++ models/Snack.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////
+// Our Models
+/////////////////////////////////////////////
+// pull schema and model from mongoose using object destructuring
+const { Schema, model } = mongoose;
+
+// make snack schema
+const snackSchema = new Schema({
+    name: String,
+    cost: Number,
+    calories: Number
+});
+
+// make snack model
+const Snack = model('Snack', snackSchema)
+
+// export model
+module.exports = Snack;
